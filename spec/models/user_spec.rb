722a802e835754e95e6304b88bf5efb37ca2c53f
Nikola Begedin <begedinnikola@gmail.com>
2015-09-28 15:32:42 +0200

    Added tests lost in bad merge

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,4 +24,43 @@ describe User do
   it "is valid without a last_name" do
     expect(build(:user, last_name: nil)).to be_valid
   end
+
+  it "can have followers" do
+    user = create(:user)
+    other_user_1 = create(:user)
+    other_user_2 = create(:user)
+    create(:relationship, follower: other_user_1, followed: user)
+    create(:relationship, follower: other_user_2, followed: user)
+
+    expect(user.followers.length).to eq 2
+  end
+
+  it "can have other users it follows" do
+    user = create(:user)
+    other_user_1 = create(:user)
+    other_user_2 = create(:user)
+    create(:relationship, follower: user, followed: other_user_1)
+    create(:relationship, follower: user, followed: other_user_2)
+
+    expect(user.following.length).to eq 2
+  end
+
+  it "can follow another user" do
+    user = create(:user)
+    other_user = create(:user)
+
+    user.follow(other_user)
+    expect(user.following? other_user).to be true
+  end
+
+  it "can unfollow another user" do
+    user = create(:user)
+    other_user = create(:user)
+    create(:relationship, follower: user, followed: other_user)
+
+    expect(user.following? other_user).to be true
+
+    user.unfollow(other_user)
+    expect(user.following? other_user).to be false
+  end
 end
