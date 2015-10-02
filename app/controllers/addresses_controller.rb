23ef65a9b93edaed2e6f4e0d7359b2130b8590d6
Nikola Begedin <begedinnikola@gmail.com>
2015-10-02 11:55:08 +0200

    Added test for get address api. Cannot currently serialize point

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,17 +2,18 @@ class AddressesController < ApplicationController
   before_action :doorkeeper_authorize!
 
   def index
-    render json: index_params
+    addresses = Address.within(index_params[:latitude], index_params[:longitude], index_params[:radius])
+    render json: addresses
   end
 
   def create
   end
 
   def index_params
-    lattitude = params.require(:lattitude)
+    latitude = params.require(:latitude)
     longitude = params.require(:longitude)
     radius = params.require(:radius)
-    return { lattitude: lattitude, longitude: longitude, radius: radius }
+    return { latitude: latitude, longitude: longitude, radius: radius }
   end
 
 end
--- spec/requests/api/address_spec.rb
+++ spec/requests/api/address_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
+# Used temporarily for performance measurements. Will be removed
+require 'time'
+def measure(pass = 0)
+  start_time = Time.now
+  if pass == 0
+    yield
+  else
+    pass.times {|current| result = yield(current)}
+  end
+  (Time.now - start_time) / (pass == 0 ? 1 : pass)
+end
+
 describe 'Address API' do
   describe 'POST /address' do
     it 'requires authentication' do
@@ -18,24 +30,59 @@ describe 'Address API' do
 
       let(:token) { authenticate(email: "test-user@mail.com", password: "password") }
 
-      it 'requires lattitude, longitude and radius as parameters' do
+      before do
         create(:user, email: "test-user@mail.com", password: "password")
+      end
+
+      it 'requires latitude, longitude and radius as parameters' do
+
         expect{ authenticated_get "addresses", {}, token }.to raise_error ActionController::ParameterMissing
 
         expect{ authenticated_get "addresses", {
-          lattitude: 41.233
+          latitude: 41.233
         }, token }.to raise_error ActionController::ParameterMissing
 
         expect{ authenticated_get "addresses", {
-          lattitude: 41.233,
+          latitude: 41.233,
           longitude: 42.233
         }, token }.to raise_error ActionController::ParameterMissing
 
         expect{ authenticated_get "addresses", {
-          lattitude: 41.233,
+          latitude: 41.233,
           longitude: 42.233,
           radius: 200 * 1000
         }, token }.not_to raise_error
+      end
+
+      it 'returns a list of addresses within the specified radius of the specified point' do
+        create(:address, coordinates: 'POINT(1 1)')
+        create(:address, coordinates: 'POINT(-1 -1)')
+        create(:address, coordinates: 'POINT(2 -2)')
+        create(:address, coordinates: 'POINT(-2 2)')
+        create(:address, coordinates: 'POINT(20 20)')
+
+
+        # puts measure {
+        #   for i in 0..20000
+        #     create(:address, coordinates: "POINT(1 #{1 + i * 0.001})")
+        #   end
+        # }
+
+        # puts measure {
+        #   authenticated_get "addresses", {
+        #     latitude: 0,
+        #     longitude: 0,
+        #     radius: 200 * 1000
+        #   }, token
+        # }
+
+        authenticated_get "addresses", {
+          latitude: 0,
+          longitude: 0,
+          radius: 200 * 1000
+        }, token
+
+        expect(last_response.status).to eq 200
       end
     end
   end
