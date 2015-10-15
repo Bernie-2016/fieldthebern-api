8dc91e6201d87865be70450d539226f2b57dc236
Nikola Begedin <begedinnikola@gmail.com>
2015-10-15 09:44:47 +0200

    Fixed default for best_canvas_response, addd model spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -15,4 +15,31 @@ describe Address do
     expect(addresses_in_radius).to include(second_address_in_radius)
     expect(addresses_in_radius).not_to include(third_address_outside_radius)
   end
+
+  it "has a working 'best_canvas_response' enum" do
+    address = create(:address)
+
+    expect(address.not_yet_visited?).to be true
+
+    address.not_home!
+    expect(address.not_home?).to be true
+
+    address.unknown!
+    expect(address.unknown?).to be true
+
+    address.strongly_for!
+    expect(address.strongly_for?).to be true
+
+    address.leaning_for!
+    expect(address.leaning_for?).to be true
+
+    address.undecided!
+    expect(address.undecided?).to be true
+
+    address.leaning_against!
+    expect(address.leaning_against?).to be true
+
+    address.strongly_against!
+    expect(address.strongly_against?).to be true
+  end
 end
