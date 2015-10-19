fe5dc33e7ff3c209bde748a4dfe0503ffe5ab290
Nikola Begedin <begedinnikola@gmail.com>
2015-10-19 10:57:39 +0200

    Removed address POST endpoint

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -15,25 +15,11 @@ class AddressesController < ApplicationController
     end
   end
 
-  def create
-    address = Address.new(create_params)
-
-    if address.save
-      render json: address
-    else
-      render_validation_errors address.errors
-    end
-  end
-
   def index_params
     latitude = params.require(:latitude)
     longitude = params.require(:longitude)
     radius = params.require(:radius)
     { latitude: latitude, longitude: longitude, radius: radius }
-  end
-
-  def create_params
-    params.permit(:latitude, :longitude, :street_1, :street_2, :city, :state_code, :zip_code, :visited_at)
   end
 
   def search_params
--- spec/requests/api/address_spec.rb
+++ spec/requests/api/address_spec.rb
@@ -1,48 +1,6 @@
 require "rails_helper"
 
 describe "Address API" do
-  describe "POST /address" do
-    it "requires authentication" do
-      post "#{host}/addresses"
-      expect(last_response.status).to eq 401
-    end
-
-    context "when authenticated" do
-      let(:token) { authenticate(email: "test-user@mail.com", password: "password") }
-
-      before do
-        create(:user, email: "test-user@mail.com", password: "password")
-      end
-
-      it "creates an address" do
-        authenticated_post "addresses", {
-          latitude: 41.233,
-          longitude: 42.233,
-          street_1: "A street",
-          street_2: "Something special",
-          city: "Testtown",
-          state_code: "TT",
-          zip_code: "1ABCDE",
-          latest_result: "Not home",
-        }, token
-
-        expect(last_response.status).to eq 200
-
-        new_address = Address.last
-
-        expect(new_address.persisted?).to be true
-
-        expect(new_address.latitude).to eq 41.233
-        expect(new_address.longitude).to eq 42.233
-        expect(new_address.street_1).to eq "A street"
-        expect(new_address.street_2).to eq "Something special"
-        expect(new_address.city).to eq "Testtown"
-        expect(new_address.state_code).to eq "TT"
-        expect(new_address.zip_code).to eq "1ABCDE"
-      end
-    end
-  end
-
   describe "GET /address" do
     it "requires authentication" do
       get "#{host}/addresses"
