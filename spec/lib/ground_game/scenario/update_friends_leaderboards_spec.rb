3bd01de6c63756730834768abffbb4066aae8daf
Nikola Begedin <begedinnikola@gmail.com>
2015-10-23 15:52:08 +0200

    update_friends_leaderboards_spec

--- spec/lib/ground_game/scenario/update_friends_leaderboards_spec.rb
+++ spec/lib/ground_game/scenario/update_friends_leaderboards_spec.rb
@@ -5,9 +5,22 @@ module GroundGame
   module Scenario
 
     describe UpdateFriendsLeaderboards do
-
       describe "#call" do
-        it "updates each of the user's friend's 'friends' leaderboard"
+
+        before do
+          @user = create(:user, id: 10)
+          users_friends = create_list(:user, 5)
+          users_friends.each { |friend| friend.follow(@user) }
+        end
+
+        it "updates each of the user's friend's 'friends' leaderboard" do
+          UpdateFriendsLeaderboards.new(@user).call
+
+          @user.followers.each do |follower|
+            follower_friend_leaderboard = Ranking.for_friend_list(list_owner: follower, id: @user.id)
+            expect(follower_friend_leaderboard.length).to eq 1
+          end
+        end
       end
     end
   end
