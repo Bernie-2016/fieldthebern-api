755e57bdfc0f79cdce99579c8c6a5ec2db0ce953
Eric Stiens <estiens@gmail.com>
2015-10-23 17:14:52 -0500

    simplify conditional logic for hash traversal

--- app/workers/add_facebook_profile_picture.rb
+++ app/workers/add_facebook_profile_picture.rb
@@ -6,9 +6,7 @@ class AddFacebookProfilePicture
     facebook_access_token = user.facebook_access_token
     graph = Koala::Facebook::API.new(facebook_access_token, ENV['FACEBOOK_APP_SECRET'])
     picture = graph.get_object('me?fields=picture')
-    if picture['picture'] && picture['picture']['data'] && picture['picture']['data']['url']
-      picture_url = picture['picture']['data']['url']
-    end
+    picture_url = picture['picture'].try(:[], 'data').try(:[], 'url')
     return unless picture_url
     user.photo = URI.parse(picture_url)
     user.save
