0338ab6313208ee9d7a55fcf3af0505788c6a29a
Josh Smith <joshdotsmith@gmail.com>
2015-10-23 17:18:13 -0700

    Fix specs

--- app/workers/add_facebook_profile_picture.rb
+++ app/workers/add_facebook_profile_picture.rb
@@ -5,7 +5,7 @@ class AddFacebookProfilePicture
     user = User.find(user_id)
     facebook_access_token = user.facebook_access_token
     graph = Koala::Facebook::API.new(facebook_access_token, ENV['FACEBOOK_APP_SECRET'])
-    picture_url = graph.get_picture('me', type: "original")
+    picture_url = graph.get_picture('me', type: "large")
     return unless picture_url
     user.photo = URI.parse(picture_url)
     user.save
