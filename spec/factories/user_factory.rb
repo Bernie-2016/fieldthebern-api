505c8bc77763a2a4529ebf09bf5e143234e35fbe
Nikola Begedin <begedinnikola@gmail.com>
2015-10-26 12:23:50 +0100

    Modified AddFacebookProfilePicture worker to not modify user phot from facebook if they already have one.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -6,5 +6,14 @@ FactoryGirl.define do
     f.home_state "NY"
 
     sequence(:email) { |n| "user_" + n.to_s + "@mail.com" }
+
+    trait :with_a_photo do
+      after(:build) do |user, evaluator|
+        file = File.open("#{Rails.root}/spec/sample_data/default-avatar.png", 'r')
+        base_64_image = Base64.encode64(open(file) { |io| io.read })
+        user.base_64_photo_data = base_64_image
+        user.decode_image_data
+      end
+    end
   end
 end
