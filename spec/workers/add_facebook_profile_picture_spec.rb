1af565d4b9fa4e8b9f5e1a4454d0c243562a121c
Nikola Begedin <begedinnikola@gmail.com>
2015-10-26 12:30:27 +0100

    Modified 'AddFacebookProfilePicture' worker to only do the work if the user doesn't have a photo already

--- spec/workers/add_facebook_profile_picture_spec.rb
+++ spec/workers/add_facebook_profile_picture_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
-describe AddFacebookProfilePicture do
+describe AddFacebookProfilePicture ,vcr: { cassette_name: 'workers/add profile pic/add_profile_pic_from_facebook' } do
 
   before do
     oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], ENV["FACEBOOK_REDIRECT_URL"])
     test_users = Koala::Facebook::TestUsers.new(app_id: ENV["FACEBOOK_APP_ID"], secret: ENV["FACEBOOK_APP_SECRET"])
-    facebook_user = test_users.create(true, "email,user_friends")
-    @user = create(:user, facebook_id: facebook_user["id"], facebook_access_token: facebook_user["access_token"])
+    @facebook_user = test_users.create(true, "email,user_friends")
   end
 
-  it 'adds a profile picture from facebook', vcr: { cassette_name: 'workers/add profile pic/add_profile_pic_from_facebook' } do
-    AddFacebookProfilePicture.new.perform(@user.id)
-    @user.reload
-    expect(@user.photo.path).to_not be_nil
+  context "when user already has a photo" do
+    before do
+      @user = create(:user, :with_a_photo, facebook_id: @facebook_user["id"], facebook_access_token: @facebook_user["access_token"])
+    end
+
+    it "doesn't modify the photo" do
+      AddFacebookProfilePicture.new.perform(@user.id)
+      @user.reload
+      expect(@user.photo.path).to include "original.png"
+    end
+  end
+
+  context "when user doesn't have a photo" do
+    before do
+      @user = create(:user, facebook_id: @facebook_user["id"], facebook_access_token: @facebook_user["access_token"])
+    end
+
+    it "adds a profile picture from facebook" do
+      AddFacebookProfilePicture.new.perform(@user.id)
+      @user.reload
+      expect(@user.photo.path).not_to be_nil
+      expect(@user.photo.path).not_to include "original.png"
+    end
   end
 end
