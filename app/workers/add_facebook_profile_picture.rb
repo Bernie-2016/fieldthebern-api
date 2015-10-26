c819ced3fc13b771e0d17614a5e88bf2f34b1ec5
Nikola Begedin <begedinnikola@gmail.com>
2015-10-26 12:26:56 +0100

    Revert "Modified AddFacebookProfilePicture worker to not modify user phot from facebook if they already have one."
    This reverts commit 505c8bc77763a2a4529ebf09bf5e143234e35fbe.

--- app/workers/add_facebook_profile_picture.rb
+++ app/workers/add_facebook_profile_picture.rb
@@ -3,21 +3,12 @@ class AddFacebookProfilePicture
 
   def perform(user_id)
     user = User.find(user_id)
-
-    photo_url = facebook_photo_url(user) unless user.photo.path
-
-    if photo_url
-      user.photo = URI.parse(photo_url)
-      user.save
-    end
+    facebook_access_token = user.facebook_access_token
+    graph = Koala::Facebook::API.new(facebook_access_token, ENV['FACEBOOK_APP_SECRET'])
+    picture = graph.get_object('me?fields=picture')
+    picture_url = picture['picture'].try(:[], 'data').try(:[], 'url')
+    return unless picture_url
+    user.photo = URI.parse(picture_url)
+    user.save
   end
-
-  private
-    def facebook_photo_url(user)
-      facebook_access_token = user.facebook_access_token
-      graph = Koala::Facebook::API.new(facebook_access_token, ENV['FACEBOOK_APP_SECRET'])
-      picture = graph.get_object('me?fields=picture')
-      picture_url = picture['picture'].try(:[], 'data').try(:[], 'url')
-      picture_url
-    end
 end
