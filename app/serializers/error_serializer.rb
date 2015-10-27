22086f1b1d9e2a2700947f1c9ec2667e61e51a78
Nikola Begedin <begedinnikola@gmail.com>
2015-10-27 13:35:06 +0100

    ErrorSerializer spec

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -3,26 +3,28 @@ class ErrorSerializer
     { errors: [serialize_error(error)] }
   end
 
-  def self.serialize_error(error)
-    return serialize_argument_error(error) if error.class == ArgumentError
-    return serialize_record_not_found_error(error) if error.class == ActiveRecord::RecordNotFound
-  end
+  private
 
-  def self.serialize_argument_error(error)
-    {
-      id: "ARGUMENT_ERROR",
-      title: "Argument error",
-      detail: error.message,
-      status: 422
-    }
-  end
+    def self.serialize_error(error)
+      return serialize_argument_error(error) if error.class == ArgumentError
+      return serialize_record_not_found_error(error) if error.class == ActiveRecord::RecordNotFound
+    end
 
-  def self.serialize_record_not_found_error(error)
-    return {
-      id: "RECORD_NOT_FOUND",
-      title: "Record not found",
-      detail: error.message,
-      status: 404
-    }
-  end
+    def self.serialize_argument_error(error)
+      {
+        id: "ARGUMENT_ERROR",
+        title: "Argument error",
+        detail: error.message,
+        status: 422
+      }
+    end
+
+    def self.serialize_record_not_found_error(error)
+      return {
+        id: "RECORD_NOT_FOUND",
+        title: "Record not found",
+        detail: error.message,
+        status: 404
+      }
+    end
 end
