5c178ed46c0cb4bbc04ab710ddd727ddd1f47e15
Nikola Begedin <begedinnikola@gmail.com>
2015-10-28 09:32:18 +0100

    Made VCR work with Parse requests

--- spec/lib/ground_game/parse_notification_spec.rb
+++ spec/lib/ground_game/parse_notification_spec.rb
@@ -3,20 +3,27 @@ require "ground_game/parse_notification"
 module GroundGame
   describe ParseNotification do
     describe "#send" do
-      it "can send to a user" do
+      it "can send to a user"  do
         expect_any_instance_of(Parse::Push).to receive(:save).and_call_original
 
-        response = ParseNotification.new(username: "test", message: "A message").send
+        VCR.use_cassette "lib/ground_game/parse_notification/send_to_user" do
+          response = ParseNotification.new(username: "test", message: "A message").send
+          expect(response["result"]).to be true
+        end
 
-        expect(response["result"]).to be true
       end
 
       it "can send to a channel" do
         expect_any_instance_of(Parse::Push).to receive(:save).and_call_original
 
-        response = ParseNotification.new(channel: "test", message: "A message").send
+        # TODO: Delete and re-record the tape when we have ios set-up.
+        # We need to setup an ios certificate. Right now, the tape recorded a
+        # '115: To push to ios devices, you must first configure a valid certificate.' response
+        VCR.use_cassette "lib/ground_game/parse_notification/send_to_channel" do
+          response = ParseNotification.new(channel: "test", message: "A message").send
+          expect(response["result"]).to be true
+        end
 
-        expect(response["result"]).to be true
       end
     end
   end
