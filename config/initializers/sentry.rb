7e19a8824d209a7d9c17af9513d42ac8b03e2dbf
Josh Smith <joshdotsmith@gmail.com>
2015-10-29 10:29:35 -0700

    Remove development from sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.environments = %w[ production development staging ]
+  config.environments = %w[ production staging ]
   config.dsn = ENV['SENTRY_DSN']
 end
