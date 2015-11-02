543fbf60affe4be187c5480aa634245f7927e8fb
Israel De La Hoz <israeldelahoz@gmail.com>
2015-11-01 21:41:56 -0500

    add spec for devices api

---
+++ spec/requests/api/devices_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "Devices API" do
+
+  let(:token) { authenticate(email: "test-user@mail.com", password: "password") }
+  before do
+    create(:user, email: "test-user@mail.com", password: "password")
+  end
+
+
+  it "should register a devise for a user" do
+
+
+
+    authenticated_post "devices", {
+      data: {
+        attributes: {
+          token: "123456",
+          enabled: true,
+          platform: "Android"
+        }
+      }
+    }, token
+
+    expect(json.id).not_to be_blank
+    expect(json.token).to eq "123456"
+    expect(json.platform).to eq "Android"
+    expect(json).to be_enabled
+  end
+
+end
