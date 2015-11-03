b38d741444f5c935c68eecb2c7040adcc9b55735
Nikola Begedin <begedinnikola@gmail.com>
2015-11-03 19:12:32 +0100

    Parse notifier and spec

--- lib/ground_game/parse_notifier.rb
+++ lib/ground_game/parse_notifier.rb
@@ -1,7 +1,20 @@
 module GroundGame
   class ParseNotifier
-    def self.ping(username: nil, channel: nil)
-      ParseNotification.send(username: username, channel: channel, message: "Ping!")
+    def self.ping_user(user: nil)
+      Device.where(user: user).each do |device|
+        ParseNotification.send_to_device(device_token: device.token, message: "Ping!")
+      end
+    end
+
+    def self.ping_channel(channel: nil)
+      ParseNotification.send_to_channel(channel: channel, message: "Ping!")
+    end
+
+    def self.friend_has_beaten_user(user: nil, friend: nil)
+      message = "Your friend #{friend.first_name} has beaten you!"
+      Device.where(user: user).each do |device|
+        ParseNotification.send_to_device(device_token: device.token, message: message)
+      end
     end
 
     # TODO: Define notification types we will use
--- spec/lib/ground_game/parse_notifier_spec.rb
+++ spec/lib/ground_game/parse_notifier_spec.rb
@@ -2,16 +2,35 @@ require "ground_game/parse_notifier"
 
 module GroundGame
   describe ParseNotifier do
-    describe ".ping" do
-      it "can ping a user"  do
-        expect(GroundGame::ParseNotification).to receive(:send).with(message: "Ping!", channel: nil, username: "test")
-        ParseNotifier.ping(username: "test")
+    describe ".ping_user" do
+      # it "works"  do
+      #   expect(GroundGame::ParseNotification).to receive(:send_to_device).with(message: "Ping!", device_token: "123")
+      #   user= create(:user)
+      #   create(:device, user: user, token: "123")
+      #   ParseNotifier.ping_user(user: user)
+      # end
+
+      # it "works when user has multiple devices"
+    end
+
+    describe ".ping_channel" do
+      it "works" do
+        expect(GroundGame::ParseNotification).to receive(:send_to_channel).with(message: "Ping!",  channel: "test")
+        ParseNotifier.ping_channel(channel: "test")
+      end
+    end
+
+    describe ".friend_has_beaten_user" do
+      it "works" do
+        user = create(:user)
+        friend_who_has_beaten_user = create(:user, first_name: "John")
+        create(:device, user: friend_who_has_beaten_user, token: "123")
+
+        expect(GroundGame::ParseNotification).to receive(:send_to_device).with(message: "Your friend John has beaten you!", device_token: "123")
+        ParseNotifier.friend_has_beaten_user(user: user, friend: friend_who_has_beaten_user)
       end
 
-      it "can ping a channel" do
-        expect(GroundGame::ParseNotification).to receive(:send).with(message: "Ping!", channel: "test", username: nil)
-        ParseNotifier.ping(channel: "test")
-      end
+      # it "works when user has multiple devices"
     end
   end
 end
