1f096ceb58f7b501f35cd56b9e55c184cabb08cc
Nikola Begedin <begedinnikola@gmail.com>
2015-11-03 13:38:06 +0100

    device model spec

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Device, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Device, type: :model do
+  context 'schema' do
+    it { should have_db_column(:platform).of_type(:string) }
+    it { should have_db_column(:enabled).of_type(:boolean) }
+    it { should have_db_column(:token).of_type(:string) }
+    it { should have_db_column(:user_id).of_type(:integer) }
+    it { should have_db_column(:created_at).of_type(:datetime) }
+    it { should have_db_column(:updated_at).of_type(:datetime) }
+    it { should have_db_index(:token) }
+  end
+
+  context 'associations' do
+    it { should belong_to(:user) }
+  end
 end
