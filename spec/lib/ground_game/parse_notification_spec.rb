633b4f4b5fc7a50162164cf087ba7affe8cffc96
Nikola Begedin <begedinnikola@gmail.com>
2015-11-03 18:41:50 +0100

    parse notification specs

--- spec/lib/ground_game/parse_notification_spec.rb
+++ spec/lib/ground_game/parse_notification_spec.rb
@@ -2,20 +2,29 @@ require "ground_game/parse_notification"
 
 module GroundGame
   describe ParseNotification do
-    describe ".send" do
+    describe ".send_to_device" do
       it "delegates to .new.send"  do
-        expect(GroundGame::ParseNotification).to receive(:new).with(message: "A message", username: "test", channel: nil).and_call_original
+        expect(GroundGame::ParseNotification).to receive(:new).with(message: "A message", device_token: "123").and_call_original
         expect_any_instance_of(GroundGame::ParseNotification).to receive(:send)
-        ParseNotification.send(username: "test", message: "A message")
+        ParseNotification.send_to_device(device_token: "123", message: "A message")
+      end
+    end
+
+    describe ".send_to_channel" do
+
+      it "delegates to .new.send"  do
+        expect(GroundGame::ParseNotification).to receive(:new).with(message: "A message", channel: "test").and_call_original
+        expect_any_instance_of(GroundGame::ParseNotification).to receive(:send)
+        ParseNotification.send_to_channel(channel: "test", message: "A message")
       end
     end
 
     describe "#send" do
-      it "can send to a user"  do
+      it "can send to a devices"  do
         expect(Parse::Push).to receive(:new).with({ alert: "A message"}).and_call_original
 
         VCR.use_cassette "lib/ground_game/parse_notification/send_to_user" do
-          response = ParseNotification.new(username: "test", message: "A message").send
+          response = ParseNotification.new(device_token: "test", message: "A message").send
           expect(response["result"]).to be true
         end
 
