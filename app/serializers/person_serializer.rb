8729175ac59a62f3faa9300fd3b355b3dba27eab
Nikola Begedin <begedinnikola@gmail.com>
2015-11-05 10:54:41 +0100

    Resolve remaining serializer spec

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,5 +1,7 @@
 class PersonSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :party_affiliation, :canvas_response
+  attributes :id, :first_name, :last_name,
+    :party_affiliation, :canvas_response,
+    :previously_participated_in_caucus_or_primary
 
   belongs_to :address
 end
