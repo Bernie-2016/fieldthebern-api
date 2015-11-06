56184a71ff8c49870ca9b8b39f07307949e41945
Nikola Begedin <begedinnikola@gmail.com>
2015-11-06 09:56:51 +0100

    Added specifying of custom origins, while still using wildcard (for now) if no setting for it is present

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,11 @@ module GroundGame
 
     config.middleware.use ActionDispatch::Flash
 
+    cors_origins = ENV.fetch('CORS_ORIGINS', '*')
+    puts cors_origins
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins '*'
+        origins cors_origins
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
