e70dfc8bc7425567e0f55303be19e14df872c4a3
Nikola Begedin <begedinnikola@gmail.com>
2015-11-17 09:17:49 +0100

    Changed Person.preferred_contact_method default to nil

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116230108) do
+ActiveRecord::Schema.define(version: 20151117081214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,7 +102,7 @@ ActiveRecord::Schema.define(version: 20151116230108) do
     t.datetime "updated_at"
     t.string   "email"
     t.string   "phone"
-    t.string   "preferred_contact_method",                     default: "email"
+    t.string   "preferred_contact_method"
     t.boolean  "previously_participated_in_caucus_or_primary", default: false
   end
 
