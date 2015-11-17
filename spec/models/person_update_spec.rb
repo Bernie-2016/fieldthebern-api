cdf72c66ead0c3777ca3fab73f553c188f392128
scarfdestroyer <bradyrudesill@gmail.com>
2015-11-16 21:03:56 -0800

    Make PersonUpdate model specs more explicit about their behavior

--- spec/models/person_update_spec.rb
+++ spec/models/person_update_spec.rb
@@ -50,16 +50,73 @@ describe PersonUpdate do
   describe ".create_for_visit_and_person" do
     it "creates an update with 'created' type if person is a new record" do
       visit = create(:visit)
-      person = build(:person)
+      person = build(:person,
+        first_name: "John",
+        last_name: "Doe",
+        canvas_response: :strongly_for,
+        party_affiliation: :democrat_affiliation,
+        phone: "555-555-1212",
+        preferred_contact_method: "email",
+        previously_participated_in_caucus_or_primary: true
+      )
       person_update = PersonUpdate.create_for_visit_and_person(visit, person)
       expect(person_update.created?).to be true
+
+      expect(person_update.old_first_name).to be_nil
+      expect(person_update.old_last_name).to be_nil
+      expect(person_update.old_canvas_response).to eq "unknown"
+      expect(person_update.old_party_affiliation).to eq "unknown_affiliation"
+      expect(person_update.old_phone).to be_nil
+      expect(person_update.old_preferred_contact_method).to eq "contact_by_email"
+      expect(person_update.old_previously_participated_in_caucus_or_primary).to eq false
+
+      expect(person_update.new_first_name).to eq "John"
+      expect(person_update.new_last_name).to eq "Doe"
+      expect(person_update.new_canvas_response).to eq "strongly_for"
+      expect(person_update.new_party_affiliation).to eq "democrat_affiliation"
+      expect(person_update.new_phone).to eq "555-555-1212"
+      expect(person_update.new_preferred_contact_method).to eq "contact_by_email"
+      expect(person_update.new_previously_participated_in_caucus_or_primary).to eq true
     end
 
     it "creates an update with 'modified' type if person is an existing record" do
       visit = create(:visit)
-      person = create(:person)
+      person = create(:person,
+        first_name: "John",
+        last_name: "Doe",
+        canvas_response: :strongly_for,
+        party_affiliation: :democrat_affiliation,
+        phone: "555-555-1212",
+        preferred_contact_method: "email",
+        previously_participated_in_caucus_or_primary: true
+      )
+
+      person.first_name = "Josh"
+      person.last_name = "Smith"
+      person.canvas_response = :strongly_against
+      person.party_affiliation = :republican_affiliation
+      person.phone = "123-456-7890"
+      person.preferred_contact_method = "phone"
+      person.previously_participated_in_caucus_or_primary = false
+
       person_update = PersonUpdate.create_for_visit_and_person(visit, person)
       expect(person_update.modified?).to be true
+
+      expect(person_update.old_first_name).to eq "John"
+      expect(person_update.old_last_name).to eq "Doe"
+      expect(person_update.old_canvas_response).to eq "strongly_for"
+      expect(person_update.old_party_affiliation).to eq "democrat_affiliation"
+      expect(person_update.old_phone).to eq "555-555-1212"
+      expect(person_update.old_preferred_contact_method).to eq "contact_by_email"
+      expect(person_update.old_previously_participated_in_caucus_or_primary).to eq true
+
+      expect(person_update.new_first_name).to eq "Josh"
+      expect(person_update.new_last_name).to eq "Smith"
+      expect(person_update.new_canvas_response).to eq "strongly_against"
+      expect(person_update.new_party_affiliation).to eq "republican_affiliation"
+      expect(person_update.new_phone).to eq "123-456-7890"
+      expect(person_update.new_preferred_contact_method).to eq "contact_by_phone"
+      expect(person_update.new_previously_participated_in_caucus_or_primary).to eq false
     end
   end
 end
