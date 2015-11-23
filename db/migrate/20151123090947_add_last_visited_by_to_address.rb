65ec159f05bb73f19ef36b12be69747c1740b7aa
Nikola Begedin <begedinnikola@gmail.com>
2015-11-23 11:06:17 +0100

    Add update of existing addresses to migration

--- db/migrate/20151123090947_add_last_visited_by_to_address.rb
+++ db/migrate/20151123090947_add_last_visited_by_to_address.rb
@@ -1,5 +1,13 @@
 class AddLastVisitedByToAddress < ActiveRecord::Migration
   def change
     add_column :addresses, :last_visited_by_id, :integer
+
+    Address.all.each do |address|
+      visits = AddressUpdate.where(address: address).map(&:visit)
+      if visits.length > 0
+        latest_visit = visits.sort_by{ |visit| visit.updated_at }.last
+        address.update(last_visited_by: latest_visit.user)
+      end
+    end
   end
 end
